72383334ebf64a705f6e80c707e9b80e5c344f30
geetika1723 <geetikasethi1723@gmail.com>
2022-11-05 11:59:39 +0530

    signup done

--- lib/auth1_web/controllers/page_controller.ex
+++ lib/auth1_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule Auth1Web.PageController do
   use Auth1Web, :controller
 
   def index(conn, _params) do
-    render(conn, "sample.html")
+    render(conn, "index.html")
   end
 end
--- lib/auth1_web/router.ex
+++ lib/auth1_web/router.ex
@@ -8,7 +8,7 @@ defmodule Auth1Web.Router do
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, {Auth1Web.LayoutView, :root}
-    plug :protect_from_forgery
+
     plug :put_secure_browser_headers
     plug :fetch_current_user
   end
